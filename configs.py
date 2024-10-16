8a624aaaf337088aa40af72b970034cc1da9d8b9
creatorv06 <151960008+creatorv06@users.noreply.github.com>
2024-10-17 01:22:50 +0530

    configs.py

--- configs.py
+++ configs.py
@@ -1,17 +1,17 @@
 import os
 
 class Config(object):
-  API_ID = int(os.environ.get("API_ID", "22787559"))
-  API_HASH = os.environ.get("API_HASH", "e771139d8f3d4ae57f663ad049bbb710")
-  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6957865617:AAErJV_3eAe6mQVFU8g1kvDRwUm66i3vVkE")
-  BOT_USERNAME = os.environ.get("BOT_USERNAME", "latest_tv_show_File_store_bot")
-  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002117171789"))
+  API_ID = int(os.environ.get("API_ID", "29406031"))
+  API_HASH = os.environ.get("API_HASH", "83205ba993eac9cfc888387ac03e6688")
+  BOT_TOKEN = os.environ.get("BOT_TOKEN", "6840607964:AAGOoY_ofrG-Ep42cm8VUyrVILWR7pKDAwM")
+  BOT_USERNAME = os.environ.get("BOT_USERNAME", "DRX_FiLE_STORE_BOT")
+  DB_CHANNEL = int(os.environ.get("DB_CHANNEL", "-1002197832722"))
   SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "publicearn.com")
   SHORTLINK_API = os.environ.get('SHORTLINK_API', "9a696c4ea657a9791396336c3d1d84fafe02eb74")
-  BOT_OWNER = int(os.environ.get("BOT_OWNER", "1562792211"))
+  BOT_OWNER = int(os.environ.get("BOT_OWNER", "6690846736"))
   DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://Cluster0:Cluster0@cluster0.kailvlv.mongodb.net/?retryWrites=true&w=majority")
-  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1001970358116")
-  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001881381645"))
+  UPDATES_CHANNEL = os.environ.get("UPDATES_CHANNEL", "-1002054114895")
+  LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002088755828"))
   BANNED_USERS = set(int(x) for x in os.environ.get("BANNED_USERS", "").split())
   FORWARD_AS_COPY = bool(os.environ.get("FORWARD_AS_COPY", True))
   BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", True))
@@ -32,11 +32,11 @@ Send Me any Media or File. I can Work In Channel too. Add Me to Channel with Edi
 ╰──────[ 😎 ]───────────⍟
 """
   ABOUT_DEV_TEXT = f"""
-🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [TANMAY](https://t.me/Latestshowtv)
+🧑🏻‍💻 𝗗𝗲𝘃𝗲𝗹𝗼𝗽𝗲𝗿: [𝐃𝐑𝐗](https://t.me/DRX_OFFICIALS_BOT)
  
  I am Super noob Please Support My Hard Work.
 
-[Donate Me](https://t.me/Latestshowtv)
+[𝐃𝐨𝐧𝐚𝐭𝐞 𝐌𝐞](https://t.me/DRX_OFFICIALS_BOT)
 """
   HOME_TEXT = """
 Hello, [{}](tg://user?id={})\n\nThis is a Permanent **FileStore Bot**.
